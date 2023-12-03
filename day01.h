c53208e141c99964dac276277d71276174556ca2
matze <>
2023-12-03 01:50:08 +0100

    generalized parallelization

--- day01.h
+++ day01.h
@@ -1,55 +1,58 @@
+#include <assert.h>
+#include <malloc.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
-// part 1
-//# define CHECK_COUNT 10
-// part 2
-#define CHECK_COUNT 20
+#include "helper.h"
 
-const char *digit_and_names[20] = {"0", "1", "2", "3", "4",
-                                   "5", "6", "7", "8", "9",
-                                   ".", "one", "two", "three", "four",
+const char *digit_and_names[20] = {"0",    "1",   "2",     "3",     "4",
+                                   "5",    "6",   "7",     "8",     "9",
+                                   ".",    "one", "two",   "three", "four",
                                    "five", "six", "seven", "eight", "nine"};
-const long long masks[20] = {255, 255, 255, 255,
-                             255, 255, 255, 255,
-                             255, 255, 255, 16777215,
-                             16777215, 1099511627775, 4294967295, 4294967295,
-                             16777215, 1099511627775, 1099511627775, 4294967295};
+const long long masks[20] = {
+    255,        255,      255,           255,           255,
+    255,        255,      255,           255,           255,
+    255,        16777215, 16777215,      1099511627775, 4294967295,
+    4294967295, 16777215, 1099511627775, 1099511627775, 4294967295};
 
-//check if "digit" starts at position j in buffer buff_int
-int matches(const long long *buff_int, const int j) {
-    const long long *dig_int = (long long*)(digit_and_names[j]);
-    const long long symdiff = (*buff_int) ^ (*dig_int);
-    return !((symdiff) & (masks[j]));
-
+// check if "digit" starts at position j in buffer buff_int
+inline int matches(const long long *buff_int, const int j) {
+  const long long *dig_int = (long long *)(digit_and_names[j]);
+  const long long difference = (*buff_int) ^ (*dig_int);
+  return !((difference) & (masks[j]));
 }
 
-int get_first(const char *buffer) {
-    for (int i = 0; buffer[i] != 0; i++) {
-        for (int j = 0; j < CHECK_COUNT; j++) {
-            if (matches((long long*)(buffer+i), j)) {
-                return j % 10;
-            }
-        }
+inline int get_first(const char *buffer, bool part2) {
+  for (int i = 0; buffer[i] != 0; i++) {
+    for (int j = 0; j < (part2 ? 20 : 10); j++) {
+      if (matches((long long *)(buffer + i), j)) {
+        return j % 10;
+      }
     }
+  }
+  assert(false);
 }
 
-int get_last(const char *buffer) {
-    for (int i = strlen(buffer); i >= 0; i--) {
-        for (int j = 0; j < CHECK_COUNT; j++) {
-            if (matches((long long*)(buffer+i), j)) {
-                return j % 10;
-            }
-        }
+inline int get_last(const char *buffer, bool part2) {
+  for (int i = (int)strlen(buffer); i >= 0; i--) {
+    for (int j = 0; j < (part2 ? 20 : 10); j++) {
+      if (matches((long long *)(buffer + i), j)) {
+        return j % 10;
+      }
     }
+  }
+  assert(false);
 }
 
-long long day01(char* buf) {
-    long long res = 0;
-    char** buf_pos=&buf;
-    while (*buf_pos != NULL) {
-        char* line = strsep(buf_pos,"\n");
-        res += get_first(line) * 10 + get_last(line);
-    }
-    return res;
+struct two_part_result *day01(char *buf) {
+  struct two_part_result *result = allocate_two_part_result();
+  char **buf_pos = &buf;
+  while (*buf_pos != NULL) {
+    char *line = strsep(buf_pos, "\n");
+    (result->part1_result) +=
+        get_first(line, false) * 10 + get_last(line, false);
+    (result->part2_result) += get_first(line, true) * 10 + get_last(line, true);
+  }
+  return result;
 }
--- helper.h
+++ helper.h
@@ -1,9 +1,7 @@
-//
-// Created by matze on 02/12/2023.
-//
-
 #ifndef AOCC_HELPER_H
 #define AOCC_HELPER_H
+
+#include <malloc.h>
 
 #define max(a, b)                                                              \
   ({                                                                           \
@@ -19,4 +17,36 @@
     _a < _b ? _a : _b;                                                         \
   })
 
-#endif //AOCC_HELPER_H
+struct two_part_result {
+  long long part1_result;
+  long long part2_result;
+};
+
+struct two_part_result *allocate_two_part_result() {
+  struct two_part_result *result = malloc(sizeof *result);
+  result->part1_result = 0;
+  result->part2_result = 0;
+  return result;
+}
+
+void add_consume_partial_result(struct two_part_result *result,
+                                struct two_part_result *partial_result) {
+  (result->part2_result) += (partial_result->part2_result);
+  (result->part1_result) += (partial_result->part1_result);
+  free(partial_result);
+}
+
+void print_two_part_result(struct two_part_result *result) {
+  printf("result:\npart1:\t%lld\npart2:\t%lld\n", result->part1_result,
+         result->part2_result);
+}
+
+void print_day_result(char* day,struct two_part_result *result){
+    printf("----------\n");
+    printf("%s\n",day);
+    printf("---\n");
+    print_two_part_result(result);
+    printf("----------\n\n");
+}
+
+#endif // AOCC_HELPER_H
