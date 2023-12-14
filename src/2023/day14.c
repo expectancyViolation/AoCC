cdd015536285262bbe5a99b805fcd1a3377bcc05
matze <>
2023-12-14 23:36:07 +0100

    day 14 clean

--- src/2023/day14.c
+++ src/2023/day14.c
@@ -2,10 +2,10 @@
 #include "../../res/hashmap.c/hashmap.h"
 #include "../util/parallelize.h"
 
-long long calc_load(char *col_vec, size_t len) {
+long long calc_load(char *mat, size_t len) {
   long long res = 0;
   for (size_t i = 0; i < len; i++) {
-    char *vec = col_vec + (len * i);
+    char *vec = mat + (len * i);
     for (size_t j = 0; j < len; j++) {
       if (vec[j] == 'O') {
         res += len - j;
@@ -15,47 +15,36 @@ long long calc_load(char *col_vec, size_t len) {
   return res;
 }
 
-void simulate(char *col_vec, size_t row_len) {
-  static int call_count = 0;
-  call_count += 1;
-  printf("call:%d\n", call_count);
-  for (size_t i = 0; i < row_len; i++) {
-    char *vec = col_vec + (i * row_len);
+void simulate(char *mat, size_t len) {
+  size_t *free_pos= malloc(sizeof (size_t)*len);
+  for (size_t i = 0; i < len; i++) {
+    char *vec = mat + (i * len);
 
-    int *free_pos = NULL;
     size_t lowest_free_pos = 0;
-    for (size_t j = 0; j < row_len; j++) {
+    size_t free_pos_len=0;
+    for (size_t j = 0; j < len; j++) {
       switch (vec[j]) {
       case '#':
-        cvector_clear(free_pos);
-        lowest_free_pos = 0;
+        lowest_free_pos = free_pos_len;
         break;
       case 'O': {
-        if (cvector_size(free_pos) > lowest_free_pos) {
+        if (free_pos_len > lowest_free_pos) {
           const int tar_pos = free_pos[lowest_free_pos];
-          // printf("moving %ld to %ld\n", j, tar_pos);
           lowest_free_pos += 1;
           vec[tar_pos] = vec[j];
           vec[j] = '.';
-          cvector_push_back(free_pos, j);
-        } else {
-          //          printf("no free pos: %ld,(%ld/%ld)", j, lowest_free_pos,
-          //                 cvector_size(free_pos));
+          free_pos[free_pos_len]=j;
+          free_pos_len++;
         }
       } break;
       case '.':
-        // printf("just push it\n");
-        cvector_push_back(free_pos, j);
+        free_pos[free_pos_len]=j;
+        free_pos_len++;
         break;
-      default:
-        printf("%ld", j);
-        printf("(");
-        putchar(vec[j]);
-        printf(")<----\n");
-        // assert(false);
       }
     }
   }
+  free(free_pos);
 }
 
 void transpose_square(char *m, size_t len) {
@@ -83,35 +72,6 @@ void rot90_square(char *m, size_t len) {
   flip_square(m, len);
 }
 
-// TODO: in place? (or at least 2D (cont) array) transpose + flip
-char **rot90(char **col_vec, size_t row_len) {
-  assert(cvector_size(col_vec[0]) == row_len);
-  char **rot_col_vec = malloc(row_len * sizeof(*rot_col_vec));
-  memset(rot_col_vec, 0, row_len * sizeof(*col_vec));
-  for (int i = row_len - 1; i >= 0; i--) {
-    char *old_col = col_vec[i];
-    for (size_t j = 0; j < row_len; j++) {
-      cvector_push_back(rot_col_vec[j], old_col[j]);
-    }
-  }
-  for (int i = 0; i < row_len; i++) {
-    cvector_free(col_vec[i]);
-  }
-  free(col_vec);
-  return rot_col_vec;
-}
-
-void print_col_vec(char *col_vec, size_t row_len) {
-  printf("---\n");
-  for (size_t i = 0; i < row_len; i++) {
-    char *vec = col_vec + i * row_len;
-    for (size_t j = 0; j < row_len; j++) {
-      putchar(vec[j]);
-    }
-    printf("\n");
-  }
-}
-
 typedef struct {
   u_int64_t hash;
   int cycle_count;
@@ -127,29 +87,17 @@ u_int64_t cycle_res_hash(const void *item, u_int64_t seed0, u_int64_t seed1) {
   return res->hash;
 }
 
-char *step_cycle(char *col_vec, int row_len) {
-
-  // printf("START STEP\n");
+char *step_cycle(char *mat, int len) {
   for (int i = 0; i < 4; i++) {
-    //    col_vec = rot90(col_vec, row_len);
-    // we pull left (not up) so rot first then simulate
-
-    simulate(col_vec, row_len);
-    rot90_square(col_vec, row_len);
-    // rot90_square(col_vec, row_len);
-    // rot90_square(col_vec, row_len);
-    // print_col_vec(col_vec, row_len);
-    // printf("%lld\n", calc_load(col_vec,row_len));
+    simulate(mat, len);
+    rot90_square(mat, len);
   }
-
-  // printf("DONE STEP!!!!!!!!!!!!!!!!");
-  return col_vec;
+  return mat;
 }
 
 LLTuple year23_day14(char *buf, long buf_len) {
   LLTuple res = {0, 0};
   size_t row_len = strchr(buf, '\n') - buf;
-  // inefficient: use 2D array (fixed size) instead
   char *col_vec = malloc(sizeof(char) * row_len * row_len);
   int j = 0;
   while (buf != NULL) {
@@ -159,26 +107,24 @@ LLTuple year23_day14(char *buf, long buf_len) {
     }
     j++;
   }
+  // rotate into place (simulate simulates falling to the "left")
   rot90_square(col_vec, row_len);
   rot90_square(col_vec, row_len);
   rot90_square(col_vec, row_len);
-  // printf("%lld\n", calc_load(col_vec,row_len));
   struct hashmap *map = hashmap_new(sizeof(CycleRes), 0, 0, 0, cycle_res_hash,
                                     cycle_res_compare, NULL, NULL);
   int cycles_to_go = 1000000000;
   int cc = 0;
   while (cycles_to_go > 0) {
-    // assert(cc < 3);
     col_vec = step_cycle(col_vec, row_len);
     cc++;
     uint64_t hash_val =
-        hashmap_sip(col_vec, (sizeof(char)) * row_len * row_len, 123, 345667);
+        hashmap_sip(col_vec, (sizeof(char)) * row_len * row_len, 0, 0);
     CycleRes cycle_res = {.cycle_count = cc, .hash = hash_val};
 
     const CycleRes *old = hashmap_get(map, &(CycleRes){.hash = hash_val});
     if (old != NULL) {
       cycles_to_go = cycles_to_go % (cc - old->cycle_count);
-      printf("\t\t\ttogo %d %d\n", cycles_to_go, (cc - old->cycle_count));
     }
     hashmap_set(map, &cycle_res);
     cycles_to_go--;
@@ -191,9 +137,8 @@ LLTuple year23_day14(char *buf, long buf_len) {
 AocDayRes solve_year23_day14(const char *input_file) {
   char *input_buffer;
   const long filesize = read_file_to_memory(input_file, &input_buffer, true);
-  // printf("%s\n",input_buffer);
 
-  // need new align for parallel
+
   //  LLTuple res =
   //      parallelize(year23_day12, ll_tuple_add, input_buffer, filesize, 0);
 
